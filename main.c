4c13f02ec57750e609e71594529347a39c3f23a1
Jacopx <jacoponasi@RazorMBP.local>
2016-11-04 20:12:31 +0100

    init commit

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  2048
+//
+//  Created by Jacopo Nasi on 04/11/2016.
+//  Copyright © 2016 Jacopo Nasi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
